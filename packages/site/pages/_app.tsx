d64dc6081a6f75570cd83a8eb87ad053bee55f89
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-11-27 15:53:09 +0100

    add basic plausible integration

--- packages/site/pages/_app.tsx
+++ packages/site/pages/_app.tsx
@@ -1,12 +1,15 @@
+import PlausibleProvider from "next-plausible";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <PlausibleProvider domain="reactjobs.com">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </PlausibleProvider>
   );
 }
 
