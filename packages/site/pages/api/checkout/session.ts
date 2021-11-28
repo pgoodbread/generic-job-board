dfbbafd40b2492c8f7b013007128809a5817478d
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-11-28 12:04:24 +0100

    integrate stripe checkout

--- packages/site/pages/api/checkout/session.ts
+++ packages/site/pages/api/checkout/session.ts
@@ -1,5 +1,8 @@
+import { MongoClient } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
+import { BaseJob, JobForForm } from "../../../types";
+import { convertToBaseJob } from "../../../utils";
 
 export default async function handleStripeCheckout(
   req: NextApiRequest,
@@ -24,6 +27,26 @@ export default async function handleStripeCheckout(
     success_url: `${req.headers.origin}/checkout/result?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${req.headers.origin}/jobs/create`,
   });
+  const jobForForm: JobForForm = JSON.parse(req.body);
+
+  const baseJob = convertToBaseJob(jobForForm, session.id);
+
+  saveJobPost(baseJob);
 
   res.status(200).json({ url: session.url! });
+}
+
+async function saveJobPost(job: BaseJob) {
+  const client = new MongoClient(process.env.MONGO_URI!);
+
+  try {
+    await client.connect();
+
+    const db = client.db("generic_job_board");
+    const collection = db.collection("jobs");
+
+    await collection.insertOne(job);
+  } finally {
+    await client.close();
+  }
 }
---
+++ packages/site/pages/api/jobs/update.ts
@@ -0,0 +1,38 @@
+import { MongoClient } from "mongodb";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { BaseJob } from "../../../types";
+
+export default async function handleJobPostUpdate(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return;
+  }
+
+  const stripeJobData: Pick<BaseJob, "sessionId" | "email"> = JSON.parse(
+    req.body
+  );
+
+  const client = new MongoClient(process.env.MONGO_URI!);
+
+  try {
+    await client.connect();
+
+    const db = client.db("generic_job_board");
+    const collection = db.collection("jobs");
+
+    await collection.updateOne(
+      { sessionId: stripeJobData.sessionId },
+      {
+        $set: {
+          email: stripeJobData.email,
+        },
+      }
+    );
+  } finally {
+    await client.close();
+  }
+
+  res.status(200).json({ updateResult: "success" });
+}
