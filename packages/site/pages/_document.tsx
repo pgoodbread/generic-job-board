1e3395909b3d1f726d773f5992c08a7459f11e37
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2021-12-07 17:52:53 +0100

    move fonts to Head

--- packages/site/pages/_document.tsx
+++ packages/site/pages/_document.tsx
@@ -4,7 +4,16 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
